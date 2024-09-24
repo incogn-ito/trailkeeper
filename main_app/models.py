a9059189b810c8d48a4894239fa89d61af934277
Ito Aghayere <itohan.aghayere@gmail.com>
2024-09-23 23:21:48 -0700

    completes MVP with delete functionality and styling

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Define the possible choices for the category and creature type fields
@@ -32,7 +32,7 @@ class Goal(models.Model):
     creature_type = models.CharField(max_length=10, choices=CREATURE_CHOICES)
     target_date = models.DateField()
     description = models.TextField(max_length=250, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
