7a1363ce84edacf17239b93966227e95352e35d0
Ito Aghayere <itohan.aghayere@gmail.com>
2024-09-24 22:09:43 -0700

    fixes calendar function in the edit goal view

--- main_app/static/js/goal-form.js
+++ main_app/static/js/goal-form.js
@@ -1,7 +1,7 @@
 const dateInput = document.getElementById('id_target_date')
 
 const picker = MCDatepicker.create({
-  el: '#id_date',
+  el: '#id_target_date',
   dateFormat: 'yyyy-mm-dd',
   closeOnBlur: true,
   selectedDate: new Date(),
